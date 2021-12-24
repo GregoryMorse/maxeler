a5bae1175f86557222145be705dc61338dcddccc
Gregory Morse <gregory.morse@live.com>
2021-12-24 18:08:40 +0100

    Update ZOnePermanentCalculatorDFE.cpp

--- piquassoboost/sampling/source/ZOnePermanentCalculatorDFE.cpp
+++ piquassoboost/sampling/source/ZOnePermanentCalculatorDFE.cpp
@@ -28,7 +28,8 @@ void ZOnePermanentCalculatorDFE(std::vector<uint64_t>& matrix_mtx, std::vector<u
                 ROWCOL(matrix_mtx, 0, 1) * ROWCOL(matrix_mtx, 1, 0) * ROWCOL(matrix_mtx, 2, 2) +
                 ROWCOL(matrix_mtx, 0, 0) * ROWCOL(matrix_mtx, 1, 2) * ROWCOL(matrix_mtx, 2, 1);
      return; } //aei + bfg + cdh + ceg + bdi + afh
-    while (matrix_mtx.size() < MAX_SIM_DIM) {
+    size_t max_dim = useDual ? MAX_FPGA_DIM : MAX_SINGLE_FPGA_DIM;
+    while (matrix_mtx.size() < max_dim) {
       matrix_mtx.push_back(1UL << matrix_mtx.size());
     }
     calcPermanentZOneDFE( (const uint64_t*)matrix_mtx.data(), rows, cols, perm.data(), isSim, isGray, isRows, useGlynn, useDual);
