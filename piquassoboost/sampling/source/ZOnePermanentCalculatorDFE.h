82d0ace201c2fca9e931bc365c574236d18d8481
Gregory Morse <gregory.morse@live.com>
2021-12-24 18:08:59 +0100

    Update ZOnePermanentCalculatorDFE.h

--- piquassoboost/sampling/source/ZOnePermanentCalculatorDFE.h
+++ piquassoboost/sampling/source/ZOnePermanentCalculatorDFE.h
@@ -7,9 +7,8 @@
 #endif
 
 // the maximal dimension of matrix to be ported to FPGA for permanent calculation
-#define MAX_FPGA_DIM 40
-#define MAX_SINGLE_FPGA_DIM 28
-#define MAX_SIM_DIM 20
+#define MAX_FPGA_DIM 32
+#define MAX_SINGLE_FPGA_DIM 20
 //#define PermanentZOneRowsGrayDFE_MTXSIZE (20)
 
 extern "C"
