93ff16cebf6834ec0022f049ada0edbca33fa90a
Gregory Morse <gregory.morse@live.com>
2021-12-24 17:31:13 +0100

    Update ZOnePermanentCalculatorDFE.cpp

--- piquassoboost/sampling/source/ZOnePermanentCalculatorDFE.cpp
+++ piquassoboost/sampling/source/ZOnePermanentCalculatorDFE.cpp
@@ -8,6 +8,7 @@
 
 namespace pic {
 
+#define ROWCOL(m, r, c) ((m[r] & (1<<c)) != 0)
 
 /**
 @brief Wrapper function to call the calculate the Permanent on a DFE
@@ -18,7 +19,15 @@ void ZOnePermanentCalculatorDFE(std::vector<uint64_t>& matrix_mtx, std::vector<u
     size_t cols = rows;
     if (rows == 0) { perm[0] = 1; return; }
     else if (rows == 1) { perm[0] = matrix_mtx[0]; return; }
-    else if (rows == 2) { perm[0] = (matrix_mtx[0] & 1) * (matrix_mtx[1] >> 1) + (matrix_mtx[0] >> 1) * (matrix_mtx[1] & 1); return; }
+    else if (rows == 2) { perm[0] = ROWCOL(matrix_mtx, 0, 0) * ROWCOL(matrix_mtx, 1, 1) + ROWCOL(matrix_mtx, 0, 1) * ROWCOL(matrix_mtx, 1, 0); return; } //ad + bc
+    else if (rows == 3 && useDual) {
+      perm[0] = ROWCOL(matrix_mtx, 0, 0) * ROWCOL(matrix_mtx, 1, 1) * ROWCOL(matrix_mtx, 2, 2) +
+                ROWCOL(matrix_mtx, 0, 1) * ROWCOL(matrix_mtx, 1, 2) * ROWCOL(matrix_mtx, 2, 0) +
+                ROWCOL(matrix_mtx, 0, 2) * ROWCOL(matrix_mtx, 1, 0) * ROWCOL(matrix_mtx, 2, 1) +
+                ROWCOL(matrix_mtx, 0, 2) * ROWCOL(matrix_mtx, 1, 1) * ROWCOL(matrix_mtx, 2, 0) +
+                ROWCOL(matrix_mtx, 0, 1) * ROWCOL(matrix_mtx, 1, 0) * ROWCOL(matrix_mtx, 2, 2) +
+                ROWCOL(matrix_mtx, 0, 0) * ROWCOL(matrix_mtx, 1, 2) * ROWCOL(matrix_mtx, 2, 1);
+     return; } //aei + bfg + cdh + ceg + bdi + afh
     while (matrix_mtx.size() < MAX_SIM_DIM) {
       matrix_mtx.push_back(1UL << matrix_mtx.size());
     }
