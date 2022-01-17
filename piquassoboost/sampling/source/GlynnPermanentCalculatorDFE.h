38a8c606e02e90702c528267154c5e3faf244351
Gregory Morse <gregory.morse@live.com>
2022-01-17 17:12:53 +0100

    Update GlynnPermanentCalculatorDFE.h

--- piquassoboost/sampling/source/GlynnPermanentCalculatorDFE.h
+++ piquassoboost/sampling/source/GlynnPermanentCalculatorDFE.h
@@ -12,14 +12,15 @@
 #define MAX_FPGA_DIM 8*2
 #define MAX_SINGLE_FPGA_DIM 4*4
 
-typedef void(*CALCPERMGLYNNDFE)(const pic::Complex16*, const double*, const uint64_t, const uint64_t, pic::Complex16*);
-typedef void(*INITPERMGLYNNDFE)(int);
-typedef void(*FREEPERMGLYNNDFE)(void);
-extern "C" CALCPERMGLYNNDFE calcPermanentGlynnDFE; 
-extern "C" INITPERMGLYNNDFE initialize_DFE; 
-extern "C" FREEPERMGLYNNDFE releive_DFE; 
-
 namespace pic {
+
+/// @brief Structure type representing 16 byte complex numbers
+typedef struct ComplexFix16 {
+  /// the real part of a complex number
+  __int64_t real;
+  /// the imaginary part of a complex number
+  __int64_t imag;
+} ComplexFix16;
 
 /**
 @brief Wrapper function to call the calculate the Permanent on a DFE
@@ -28,6 +29,12 @@ void GlynnPermanentCalculator_DFE(matrix& matrix_mtx, Complex16& perm, int useDu
 
 }
 
+typedef void(*CALCPERMGLYNNDFE)(const pic::ComplexFix16*, const double*, const uint64_t, const uint64_t, pic::Complex16*);
+typedef void(*INITPERMGLYNNDFE)(int);
+typedef void(*FREEPERMGLYNNDFE)(void);
+extern "C" CALCPERMGLYNNDFE calcPermanentGlynnDFE; 
+extern "C" INITPERMGLYNNDFE initialize_DFE; 
+extern "C" FREEPERMGLYNNDFE releive_DFE; 
 
 
 #endif
