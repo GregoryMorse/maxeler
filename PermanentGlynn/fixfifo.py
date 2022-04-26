a511a85f3837e5376690717c2f59a3f75e8c44bb
Gregory Morse <gregory.morse@live.com>
2022-04-26 17:42:44 +0200

    Create fixfifo.py

---
+++ PermanentGlynn/fixfifo.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+import os
+project = "PermRepGlynnDFE"
+pxgpath = os.path.join(project, "builds", "simulation", "*.pxg")
+import glob
+import re
+pxgfiles = glob.glob(pxgpath)
+for pxgfile in pxgfiles:
+    print("Analyzing", pxgfile)
+    tree = ET.parse(pxgfile)
+    root = tree.getroot()
+    for fifo in root.findall("./Node[@type='NodeFIFO']"):
+        print(fifo.find("Text").text + " Found", "ID:", fifo.attrib["id"])
+        for edge in root.findall("./Edge[@dst_node_id='" + fifo.attrib["id"] + "']"):
+            source = root.find("./Node[@id='" + edge.attrib["src_node_id"] + "']")
+            for st in source.find("OriginStackTrace").text.splitlines():
+                m = re.match(r".*\((.*)\.maxj:([0-9]*)\)", st)
+                if not m is None and m.group(1) in pxgfile:                
+                    print(m.group(1) + ":" + m.group(2), "Source:", source.attrib["type"])
+            if source.attrib["type"] == "NodeFIFO":
+                print("Source FIFO", "ID:", source.attrib["id"]) 
+        for edge in root.findall("./Edge[@src_node_id='" + fifo.attrib["id"] + "']"):
+            dest = root.find("./Node[@id='" + edge.attrib["dst_node_id"] + "']")
+            inp = edge.attrib["dst_node_input"]       
+            for st in dest.find("OriginStackTrace").text.splitlines():
+                m = re.match(r".*\((.*)\.maxj:([0-9]*)\)", st)
+                if not m is None and m.group(1) in pxgfile:                
+                    print(m.group(1) + ":" + m.group(2), "Destination:", dest.attrib["type"], "Input:", inp)
+            if dest.attrib["type"] == "NodeFIFO":
+                print("Destination FIFO", "ID:", dest.attrib["id"]) 
+        
