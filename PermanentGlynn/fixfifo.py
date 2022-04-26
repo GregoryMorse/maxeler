c871c2d56d2a64a618b20b1cd99245b585c85f86
Gregory Morse <gregory.morse@live.com>
2022-04-26 17:53:50 +0200

    Update fixfifo.py

--- PermanentGlynn/fixfifo.py
+++ PermanentGlynn/fixfifo.py
@@ -19,6 +19,16 @@ for pxgfile in pxgfiles:
                     print(m.group(1) + ":" + m.group(2), "Source:", source.attrib["type"])
             if source.attrib["type"] == "NodeFIFO":
                 print("Source FIFO", "ID:", source.attrib["id"]) 
+            for e in root.findall("./Edge[@src_node_id='" + source.attrib["id"] + "']"):
+                dest = root.find("./Node[@id='" + e.attrib["dst_node_id"] + "']")
+                if dest.attrib["id"] == fifo.attrib["id"]: continue
+                inp = e.attrib["dst_node_input"]
+                for st in dest.find("OriginStackTrace").text.splitlines():
+                    m = re.match(r".*\((.*)\.maxj:([0-9]*)\)", st)
+                    if not m is None and m.group(1) in pxgfile:                
+                        print(m.group(1) + ":" + m.group(2), "NOT DESTINATION:", dest.attrib["type"], "Input:", inp)
+                if dest.attrib["type"] == "NodeFIFO":
+                    print("NOT DESTINATION FIFO", "ID:", dest.attrib["id"]) 
         for edge in root.findall("./Edge[@src_node_id='" + fifo.attrib["id"] + "']"):
             dest = root.find("./Node[@id='" + edge.attrib["dst_node_id"] + "']")
             inp = edge.attrib["dst_node_input"]       
