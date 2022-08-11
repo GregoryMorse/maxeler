35ca8c831d4688c3797c25f4290e269b62ddca2d
Gregory Morse <gregory.morse@live.com>
2022-08-11 21:16:22 +0200

    Update libPermanentGlynnDFE.c

--- PermanentGlynn/PermanentGlynnCPU/src/libPermanentGlynnDFE.c
+++ PermanentGlynn/PermanentGlynnCPU/src/libPermanentGlynnDFE.c
@@ -352,6 +352,7 @@ union {
 	printf("Start permanent calulation on DFE\n");
 #endif
 
+    max_config_set_int64(MAX_CONFIG_PCIE_TIMEOUT, 30+(1ULL<<(numOfPartialPerms-1-BASEKERNPOW2))/(FREQ*100000ULL));
     max_config_set_int64(MAX_CONFIG_ACTION_TIMEOUT, 30+(1ULL<<(numOfPartialPerms-1-BASEKERNPOW2))/(FREQ*100000ULL));
      
 #if defined(DUAL) && !defined(MAXELER_SIM)
