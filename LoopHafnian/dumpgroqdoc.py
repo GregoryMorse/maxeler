4ebe6841f8582e0ea371bf35cbd6f861247ef0a6
Gregory Morse <gregory.morse@live.com>
2026-04-11 21:17:40 +0200

    Add groq_api import and documentation generation

--- LoopHafnian/dumpgroqdoc.py
+++ LoopHafnian/dumpgroqdoc.py
@@ -1,5 +1,5 @@
 def dump_groq_doc():
-    import os, pydoc, pkgutil, groq, importlib, functools, shutil
+    import os, pydoc, pkgutil, groq, groq_api, importlib, functools, shutil
     try: os.mkdir("groqdoc")
     except FileExistsError: pass
     os.chdir("groqdoc")
@@ -7,7 +7,8 @@ def dump_groq_doc():
         if isinstance(package, str):
             package = importlib.import_module(package)
         results = {}
-        for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        paths = [x for x in package.__path__ if x.startswith('/opt/groq/runtime/site-packages/groq')]
+        for loader, name, is_pkg in pkgutil.walk_packages(paths):
             full_name = package.__name__ + '.' + name
             try:
                 results[full_name] = importlib.import_module(full_name)
@@ -23,6 +24,8 @@ def dump_groq_doc():
         pydoc.writedoc(module)
     groq.__all__ = dir(groq)
     pydoc.writedoc(groq)
+    groq_api.__all__ = dir(groq_api)
+    pydoc.writedoc(groq_api)
     os.chdir("..")
     shutil.make_archive("groqdoc", 'zip', "groqdoc")
 dump_groq_doc()
