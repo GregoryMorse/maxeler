3a2bacc35efdb4e2a8d472b8c1f3964a262c74cc
Gregory Morse <gregory.morse@live.com>
2026-04-12 11:31:40 +0200

    Add runbs.sh script to execute benchmark

---
+++ runbs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+env LD_LIBRARY_PATH=/home/$USER/workspace/PermanentGlynnCPU/dist/release/lib:/home/$USER/workspace/PermRepGlynnCPU/dist/release/lib:$LD_LIBRARY_PATH python3 boson_sampling_benchmark.py
+
